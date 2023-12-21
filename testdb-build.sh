3b63705cbf4ce27eb02aea3625e09b1062c991e0
Paul Sturm <paul.sturm@project-a.com>
2023-12-21 15:51:01 +0100

    Adjust docker-compose command

--- testdb-build.sh
+++ testdb-build.sh
@@ -13,13 +13,13 @@ image="${image_name}:${tag}"
 
 
 echo "## Ensure previous data is removed .."
-docker compose -f docker-compose.yml down
+docker-compose --file docker-compose.yml down
 
 echo "## Trigger build .."
-docker compose -f docker-compose.yml build
+docker-compose --file docker-compose.yml build
 
 echo "## Starting database and executing migrations .."
-docker compose -f docker-compose.yml up \
+docker-compose --file docker-compose.yml up \
     --abort-on-container-exit \
     --exit-code-from db-init
 
